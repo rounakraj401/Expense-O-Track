ddea76b64d92fec240feeee2b6f7e0015a90c4b1
Rounak Raj <rounakrajg401@gmail.com>
2025-09-15 17:48:15 +0530

    CORS issue resolved

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,9 +9,18 @@ dotenv.config(); // ✅ Load variables from .env
 const app: Express = express();
 const port = process.env.PORT || 3001;
 const mongoURI: string = process.env.MONGO_URI || "";
+const allowedOrigins = [
+  "http://localhost:5173",                       // local frontend
+  "https://expense-o-track.vercel.app"           // deployed frontend
+];
+
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: allowedOrigins,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 
 mongoose
   .connect(mongoURI)
